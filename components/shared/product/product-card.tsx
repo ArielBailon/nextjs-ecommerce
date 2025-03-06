c80ec91246ce1670815bca523ba6a77794092f59
abailonn <arielbailonn@outlook.es>
2025-03-06 17:40:20 -0500

    Day 1: App creationg and basic layout

---
+++ components/shared/product/product-card.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import ProductPrice from "./product-price";
+
+const ProductCard = ({ product }: { product: any }) => {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            priority={true}
+          />
+        </Link>
+      </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <div className="text-xs">{product.brand}</div>
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-sm front-medium">{product.name}</h2>
+        </Link>
+        <div className="flex-between">
+          <p>{product.rating} Stars</p>
+          {product.stock > 0 ? (
+            <ProductPrice value={Number(product.price)} />
+          ) : (
+            <p className="text-destructive">Out of Stock</p>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
